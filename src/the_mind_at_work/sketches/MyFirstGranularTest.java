120fad9fb38e194f1c2c5d604953504669827281
Ollie Bown <ollie@icarus.nu>
2020-09-23 15:46:46 +1000

    Some more thinking about the model and mapping to renderer.

--- src/the_mind_at_work/sketches/MyFirstGranularTest.java
+++ src/the_mind_at_work/sketches/MyFirstGranularTest.java
@@ -1,20 +1,44 @@
 package the_mind_at_work.sketches;
 
+import de.sciss.net.OSCListener;
+import de.sciss.net.OSCMessage;
 import net.happybrackets.core.HBAction;
 import net.happybrackets.device.HB;
 import net.happybrackets.sychronisedmodel.Renderer;
 import the_mind_at_work.renderers.GenericSampleAndClockRenderer;
 
 import java.lang.invoke.MethodHandles;
+import java.net.SocketAddress;
 
 public class MyFirstGranularTest implements HBAction {
     @Override
     public void action(HB hb) {
         hb.reset(); //Clears any running code on the device
 
-        GenericSampleAndClockRenderer r = new GenericSampleAndClockRenderer(hb);
-
         RendererController.setRenderer(GenericSampleAndClockRenderer.class);
+
+       //if the model is running on the Pi....
+        Model myModel = new Whatever();
+        myModel.setRunOnPi();
+        myModel.setParams();
+
+
+        RenderController.addClockTickListener() {
+            myModel.update();
+
+
+            RendererController.renderers.forEach(/something here!/) {
+
+                GenericSampleAndClockRenderer myR = (GenericSampleAndClockRenderer)r;
+
+                //grab stuff from the model
+                x = model.getFieldIntensityAt(r.getLocation());
+
+                //set the renderers
+                myR.setPitch(x);
+            };
+        };
+
 
 
 
